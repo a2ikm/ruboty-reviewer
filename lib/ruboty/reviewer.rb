a915a5379237660ef16b09a3d8f4c9c04d208faa
Masato Ikeda <masato.ikeda@gmail.com>
2016-11-28 01:16:00 +0900

    Initial commit

---
+++ lib/ruboty/reviewer.rb
@@ -0,0 +1,7 @@
+require "ruboty/reviewer/version"
+
+module Ruboty
+  module Reviewer
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/reviewer/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Reviewer
+    VERSION = "0.1.0"
+  end
+end
